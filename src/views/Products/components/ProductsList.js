91a1d5f40e60103f98e5d4bd59217a065bfd723e
Ramiro Corbalan <rcorbalan@gmail.com>
2022-05-25 14:08:41 -0300

    cambios en carousel

--- src/views/Products/components/ProductsList.js
+++ src/views/Products/components/ProductsList.js
@@ -1,6 +1,13 @@
 //import ProductCarousel from "./ProductCarousel";
 
+import React, { useState } from "react";
+
 function ProductsList(props) {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <div className="col-12 col-lg-8">
       <div
@@ -9,57 +16,34 @@ function ProductsList(props) {
         data-bs-ride="carousel"
       >
         <div className="carousel-indicators">
-          <button
+          {/*           <button
             type="button"
             data-bs-target="#carouselProduct"
             data-bs-slide-to="0"
             className="active"
             aria-current="true"
             aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselProduct"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselProduct"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+          ></button> */}
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="carousel-caption align-items-center">
-              <div className="col-12 text-center">
-                <h4>Sed ut perspiciatis</h4>
-                <h2>Nemo Enim</h2>
-                <p>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam.
-                </p>
+        {props.products.map((product, index) => {
+          return (
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className="carousel-caption align-items-center">
+                  <div className="col-12 text-center">
+                    <h4>{product.name}</h4>
+                    <h2>{product.description}</h2>
+                    {/*<p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam.
+                  </p> */}
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="carousel-item">
-            <div className="carousel-caption">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <div className="carousel-caption">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </div>
-        </div>
+          );
+        })}
+
         <button
           className="carousel-control-prev"
           type="button"
