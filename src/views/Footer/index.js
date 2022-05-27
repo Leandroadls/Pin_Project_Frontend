afe096e52e326d946a47a84c934900c76f45c223
Leandroadls <leandrodls@gmail.com>
2022-05-27 20:46:55 -0300

    last  update

---
+++ src/views/Footer/index.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <section className="footer">
+            <div>
+                <p>2020 © All rights reserved.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Footer;
