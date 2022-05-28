e2ab7ce85a8ea1b468aa938ee487c340755504b4
Leandroadls <leandrodls@gmail.com>
2022-05-28 12:43:36 -0300

    footer update

--- src/views/Footer/index.js
+++ src/views/Footer/index.js
@@ -1,8 +1,11 @@
+import logoImg from "../../images/top-logo.png";
+
 function Footer() {
     return (
-        <section className="footer">
-            <div>
-                <p>2020 © All rights reserved.</p>
+        <section className="section-footer">
+            <div className="footer">
+                <div><p>2020 © All rights reserved.</p></div>
+                <div className="botton-logo"><img src={logoImg}/></div>
             </div>
         </section>
     );
